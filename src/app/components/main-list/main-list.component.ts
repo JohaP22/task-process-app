f1ebfebfb7b95a539117a743ecaaaf6cbe185c1e
joha <joharap_22@hotmail.com>
2024-09-28 15:33:25 -0400

    add: main list

---
+++ src/app/components/main-list/main-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { TaskListComponent } from '../task-list/task-list.component';
+import { TaskComponent } from '../task/task.component';
+import { Task } from 'src/app/models/task';
+import { SkillService } from 'src/app/services/skill.service';
+
+@Component({
+  selector: 'app-main-list',
+  templateUrl: './main-list.component.html',
+  styleUrls: ['./main-list.component.css'],
+  standalone: true,
+  imports:[TaskListComponent, TaskComponent]
+})
+export class MainListComponent {
+  /** List of task. */
+  taskList: Task[] = [];
+
+  constructor(public taskService: TaskService, public skillService: SkillService) {}
+
+  /**Init lifecycle. */
+  ngOnInit(): void {
+    this.taskList = this.taskService.getTaskList()
+    this.subscribeTaskData$();
+    console.log( this.taskList);
+  }
+
+   /**
+   *
+   */
+ subscribeTaskData$(): void {
+  this.taskService.taskSubject$.pipe().subscribe((allSkills) => {
+    console.log('llego', allSkills);
+    this.taskList = allSkills;
+  });
+}
+}
