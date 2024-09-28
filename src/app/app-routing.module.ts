7cb5e97d60c5192a6b337a6073e6fcd39f32b2ff
joha <joharap_22@hotmail.com>
2024-09-28 15:28:32 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    loadComponent: () =>
+      import('./components/main-list/main-list.component').then((x) => x.MainListComponent),
+    pathMatch: 'prefix',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
